87b1a9879860227a0ca4ebdb365100fff81f9575
Nishat Munshi <nishatmunshi9@gmail.com>
2024-09-01 18:18:44 +0530

    .

--- src/main.s
+++ src/main.s
@@ -1,66 +1,43 @@
 org 0x7C00
 bits 16
 
-
 %define ENDL 0x0D, 0x0A
 
-
-start:
-    jmp main
-
-
-;
-; Prints a string to the screen
-; Params:
-;   - ds:si points to string
-;
-puts:
-    ; save registers we will modify
-    push si
-    push ax
-    push bx
-
-.loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
-    jz .done
-
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
-    int 0x10
-
-    jmp .loop
-
-.done:
-    pop bx
-    pop ax
-    pop si    
-    ret
-    
-
+section .text
 main:
     ; setup data segments
     mov ax, 0           ; can't set ds/es directly
     mov ds, ax
     mov es, ax
-    
-    ; setup stack
-    mov ss, ax
-    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
 
     ; print hello world message
-    mov si, msg_hello
-    call puts
+    mov si, msg_hello   ; move the pointer to si
+    call puts           ; call puts to print the string
 
-    hlt
-
-.halt:
+.halt:                  ; infinite loop to halt the cpu
     jmp .halt
 
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to C string
+;
+puts:
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done            ; if it is then we're done
 
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10            ; software interrupt
 
-msg_hello: db 'Hello world!', ENDL, 0
+    jmp .loop
 
+.done:  
+    ret
 
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Hello World!', ENDL, 0
+
+times 510-($-$$) db 0   ; pad with zeros upto 510 bytes
+dw 0AA55h               ; put the required bios signature
